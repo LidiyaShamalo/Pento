b96b12aa14cf4e099dfad41ce99f2463c45a0279
LidiyaShamalo <arkteya07@gmail.com>
2026-04-09 19:31:10 +0300

    creating a winning scenario

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,7 +2,8 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Сделайте предположение:", time: time())}
+    num_guess = Enum.random(1..10)
+    {:ok, assign(socket, score: 0, message: "Сделайте предположение:", num_guess: num_guess)}
 
   end
 
@@ -12,7 +13,6 @@ defmodule PentoWeb.WrongLive do
     <h1 class="mb-4 text-4xl font-extrabold">Your score: {@score}</h1>
     <h2>
       {@message}
-      It`s <%= @time %>
     </h2>
     <br />
     <h2>
@@ -30,24 +30,35 @@ defmodule PentoWeb.WrongLive do
     """
   end
 
-  def time do
-    DateTime.utc_now()|> to_string()
-  end
 
   def handle_event("guess", %{"number" => guess}, socket) do
-    message = "Your guess: #{guess}. Wrong. Guess again"
-    score = socket.assigns.score - 1
+    guess_int = String.to_integer(guess)
+    if socket.assigns.num_guess == guess_int do
+      message = "Your guess: #{guess}. Hooray! You guessed it!"
+      score = socket.assigns.score + 1
 
-    {
-      :noreply,
-      assign(
-        socket,
-        message: message,
-        score: score,
-        time: time()
-      )
-    }
+      {
+        :noreply,
+        assign(
+          socket,
+          message: message,
+          score: score,
+          num_guess: Enum.random(1..10)
+        )
+      }
+    else
+      message = "Your guess: #{guess}. Wrong. Guess again"
+      score = socket.assigns.score - 1
 
+      {
+        :noreply,
+        assign(
+          socket,
+          message: message,
+          score: score,
+          num_guess: Enum.random(1..310)
+        )
+      }
+    end
   end
-
 end
