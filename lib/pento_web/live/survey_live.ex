11baf887cc1f34bd589959fe1c1a45ddbb2c0dd6
LidiyaShamalo <arkteya07@gmail.com>
2026-04-16 19:29:00 +0300

    Update the Rating Index

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -34,9 +34,25 @@ defmodule PentoWeb.SurveyLive do
     {:noreply, socket}
   end
 
+  @impl true
+  def handle_info({:created_rating, product, product_index}, socket) do
+    socket = handle_rating_created(socket, product, product_index)
+    {:noreply, socket}
+  end
+
   defp handle_demographic_created(socket, demographic) do
     socket
     |> put_flash(:info, "Demographic created successfully")
     |> assign(:demographic, demographic)
   end
+
+  defp handle_rating_created(socket, product, product_index) do
+    current_products = socket.assigns.products
+    products = List.replace_at(current_products, product_index, product)
+
+    socket
+    |> put_flash(:info, "Рейтинг успешно создан")
+    |> assign(:products, products)
+  end
+
 end
