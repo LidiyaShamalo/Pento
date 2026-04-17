1ac6fd4241fa1e59bd83a598d24cc66e1af3ae2e
LidiyaShamalo <arkteya07@gmail.com>
2026-04-17 16:09:03 +0300

    customizing the display of graphs and charts

--- lib/pento_web/live/admin/survey_results_live.ex
+++ lib/pento_web/live/admin/survey_results_live.ex
@@ -7,6 +7,8 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
     socket
     |> assign(assigns)
     |> assign_products_with_average_ratings()
+    |> assign_dataset()
+    |> assign_chart()
     }
   end
 
@@ -17,5 +19,29 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
       Catalog.products_with_average_ratings()
     )
   end
+
+  def assign_dataset(
+    %{assigns: %{
+      products_with_average_ratings: products_with_average_ratings}
+    } = socket) do
+      socket
+      |> assign(
+        :dataset,
+        make_bar_chat_dataset(products_with_average_ratings)
+      )
+    end
+
+    defp make_bar_chat_dataset(data) do
+      Contex.Dataset.new(data)
+    end
+
+    defp assign_chart(%{assigns: %{dataset: dataset}} = socket) do
+      socket
+      |> assign(:chart, make_bar_chart(dataset))
+    end
+
+    defp make_bar_chart(dataset) do
+      Contex.BarChart.new(dataset)
+    end
 
 end
