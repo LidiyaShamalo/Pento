86ac59d64b8c582f1d4343a29d9021d20dd2e3e0
LidiyaShamalo <arkteya07@gmail.com>
2026-04-20 12:05:53 +0300

    saving the state of the filter selected by the user.

--- lib/pento_web/live/admin/survey_results_live.ex
+++ lib/pento_web/live/admin/survey_results_live.ex
@@ -5,36 +5,63 @@ defmodule PentoWeb.Admin.SurveyResultsLive do
 
   def update(assigns, socket) do
     {:ok,
-     socket
-     |> assign(assigns)
-     |> assign_age_group_filter()
-     |> assign_products_with_average_ratings()
-     |> assign_dataset()
-     |> assign_chart()
-     |> assign_chart_svg()}
+      socket
+      |> assign(assigns)
+      |> assign_age_group_filter()
+      |> assign_gender_filter()
+      |> assign_products_with_average_ratings()
+      |> assign_dataset()
+      |> assign_chart()
+      |> assign_chart_svg()}
   end
 
-  def assign_age_group_filter(socket, age_group \\ "all") do
+  def assign_age_group_filter(
+    %{assigns: %{age_group_filter: age_group_filter}} = socket
+    ) do
+    assign(socket, :age_group_filter, age_group_filter)
+  end
+
+  def assign_age_group_filter(socket) do
     socket
-    |> assign(:age_group_filter, age_group)
+    |> assign(:age_group_filter, "all")
+  end
+
+  def assign_gender_filter(
+        %{assigns: %{gender_filter: gender_filter}} = socket
+      ) do
+    assign(socket, :gender_filter, gender_filter)
+  end
+
+  def assign_gender_filter(socket) do
+    assign(socket, :gender_filter, "all")
   end
 
   def assign_products_with_average_ratings(
-        %{assigns: %{age_group_filter: age_group_filter}} =
-          socket
-      ) do
+        %{assigns: %{age_group_filter: age_group_filter, gender_filter: gender_filter}} =
+        socket) do
     assign(
       socket,
       :products_with_average_ratings,
-      Catalog.products_with_average_ratings(%{age_group_filter: age_group_filter})
+      get_products_with_average_ratings(%{
+        age_group_filter: age_group_filter,
+        gender_filter: gender_filter
+      })
     )
   end
 
+  defp get_products_with_average_ratings(filters) do
+    case Catalog.products_with_average_ratings(filters) do
+      [] ->
+        Catalog.products_with_zero_ratings()
+
+      products ->
+        products
+    end
+  end
+
   def assign_dataset(
-        %{
-          assigns: %{
-            products_with_average_ratings: products_with_average_ratings
-          }
+        %{assigns: %{
+          products_with_average_ratings: products_with_average_ratings}
         } = socket
       ) do
     socket
