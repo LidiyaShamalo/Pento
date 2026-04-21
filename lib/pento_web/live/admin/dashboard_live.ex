ed0229df9d50b6e7be9c82c8adf92199a8724e69
LidiyaShamalo <arkteya07@gmail.com>
2026-04-21 14:03:41 +0300

    Automatically update the admin panel in response to user actions

--- lib/pento_web/live/admin/dashboard_live.ex
+++ lib/pento_web/live/admin/dashboard_live.ex
@@ -1,19 +1,22 @@
 defmodule PentoWeb.Admin.DashboardLive do
+alias PentoWeb.Admin.UserActivityLive
 alias PentoWeb.Admin.SurveyResultsLive
   use PentoWeb, :live_view
   alias PentoWeb.Endpoint
 
   @survey_results_topic "survey_results"
+  @user_activity_topic "user_activity"
 
   def mount(_params, _session, socket) do
     #при повторном подключении выполняется условие
     if connected?(socket) do
       Endpoint.subscribe(@survey_results_topic)
+      Endpoint.subscribe(@user_activity_topic)
     end
     {:ok,
       socket
       |> assign(:survey_results_component_id, "survey-results")
-      |> assign(:user_activity_component_id, "user-activity")
+      |> assign(:user_activity_component_id, "user_activity")
     }
   end
 
@@ -21,6 +24,15 @@ alias PentoWeb.Admin.SurveyResultsLive
     send_update(
       SurveyResultsLive,
       id: socket.assigns.survey_results_component_id
+    )
+
+    {:noreply, socket}
+  end
+
+  def handle_info(%{event: "presence_diff"}, socket) do
+    send_update(
+      UserActivityLive,
+      id: socket.assigns.user_activity_component_id
     )
 
     {:noreply, socket}
