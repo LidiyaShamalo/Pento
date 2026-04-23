d9cf38eb173298f505a108a09c351d978019ac6c
LidiyaShamalo <arkteya07@gmail.com>
2026-04-23 15:54:17 +0300

    GAME: creating a point component

---
+++ lib/pento_web/live/game_live/component.ex
@@ -0,0 +1,34 @@
+defmodule PentoWeb.GameLive.Component do
+  use Phoenix.Component
+  alias Pento.Game.Pentomino
+  import PentoWeb.GameLive.Colors
+
+  @width 10
+
+  attr :x, :integer, required: true
+  attr :y, :integer, required: true
+  attr :fill, :string
+  attr :name, :string
+  attr :"phx-click", :string
+  attr :"phx-value", :string
+  attr :"phx-target", :any
+
+  def point(assigns) do
+    ~H"""
+    <use
+      xlink:href="#pento-point"
+      x={convert(@x)}
+      y={convert(@y)}
+      fill={@fill}
+      phx-click="pick"
+      phx-value-name={@name}
+      phx-target="#board-component"
+    />
+    """
+  end
+
+  defp convert(i) do
+    (i - 1) * @width + 2 * @width
+  end
+
+end
