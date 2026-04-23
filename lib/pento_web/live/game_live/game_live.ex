70fb5d78613b9909dd450ccd56aba4dd46909662
LidiyaShamalo <arkteya07@gmail.com>
2026-04-23 15:45:02 +0300

    GAME: example of form construction

--- lib/pento_web/live/game_live/game_live.ex
+++ lib/pento_web/live/game_live/game_live.ex
@@ -5,9 +5,21 @@ defmodule PentoWeb.GameLive do
 
   def render(assigns) do
     ~H"""
-    <section class="mx-auto max-w-4xl px-4 py-8">
-      <h1 class="font-heavytext-3xlmb-6">Добро пожаловать в Pento!</h1>
-    </section>
+    <div class="p-4"> <!-- Обертка для структуры -->
+      <section class="mx-auto max-w-4xl px-4 py-8">
+        <h1 class="text-3xl font-bold mb-6">Добро пожаловать в Pento!</h1>
+      </section>
+
+      <svg viewBox="0 0 100 100" class="w-24 h-24">
+        <defs>
+          <rect id="point"width= "10"height= "10"/>
+        </defs>
+        <use xlink:href="#point" x= "0" y= "0" fill= "blue"/>
+        <use xlink:href="#point" x= "10" y= "0" fill= "green"/>
+        <use xlink:href="#point" x= "0" y= "10" fill= "red"/>
+        <use xlink:href="#point" x= "10" y= "10" fill= "black"/>
+      </svg>
+    </div>
     """
   end
 
