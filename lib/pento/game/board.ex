a00289eb544826598fb9c9b7c87d4544fe0288cf
LidiyaShamalo <arkteya07@gmail.com>
2026-04-25 15:10:03 +0300

    GAME: adding game management functionality

--- lib/pento/game/board.ex
+++ lib/pento/game/board.ex
@@ -6,32 +6,37 @@ defmodule Pento.Game.Board do
             palette: [],
             points: []
 
-  def puzzles(), do: ~w[default wide widest medium tiny]a
+  def puzzles(), do: ~w[tiny small ball donut default wide widest medium]a
 
-  def new(palette, points) do
-    %__MODULE__{palette: palette(palette), points: points}
+  def new(palette, points, hole \\ []) do
+    %__MODULE__{palette: palette(palette), points: points -- hole}
   end
+
 
   def new(:tiny), do: new(:small, rect(5, 3))
   def new(:widest), do: new(:all, rect(20, 3))
   def new(:wide), do: new(:all, rect(15, 4))
   def new(:medium), do: new(:all, rect(12, 5))
   def new(:default), do: new(:all, rect(10, 6))
+  def new(:small), do: new(:medium, rect(7, 5))
 
-  def rect(x, y) do
-    for x <- 1..x, y <- 1..y, do: {x, y}
+  def new(:donut) do
+    new(:all, rect(8, 8), for(x <- 4..5, y <- 4..5, do: {x, y}))
   end
 
-  defp palette(:all), do: [:i, :l, :y, :n, :p, :w, :u, :v, :s, :f, :x, :t]
-  defp palette(:small), do: [:u, :v, :p]
+  def new(:ball) do
+    new(:all, rect(8, 8), for(x <- [1, 8], y <- [1, 8], do: {x, y}))
+  end
 
-  # def to_shape(board) do
-  #   Shape.__struct__(color: :purple, name: :board, points: board.points)
-  # end
+
+  def to_shape(board) do
+    Shape.__struct__(color: :purple, name: :board, points: board.points)
+  end
+
 
   def to_shapes(board) do
-    board_shape = Shape.__struct__(color: :purple, name: :board, points: board.points) #
-    #board_shape = to_shape(board)
+    board_shape = to_shape(board)
+
     pento_shapes =
       [board.active_pento | board.completed_pentos]
       |> Enum.reverse()
@@ -41,16 +46,25 @@ defmodule Pento.Game.Board do
     [board_shape | pento_shapes]
   end
 
+
   def active?(board, shape_name) when is_binary(shape_name) do
+    # ensure atoms exist
+    puzzles()
     active?(board, String.to_existing_atom(shape_name))
   end
 
   def active?(%{active_pento: %{name: shape_name}}, shape_name), do: true
-
   def active?(_board, _shape_name), do: false
 
-  def pick(board, :board), do: board
+  defp rect(x, y) do
+    for x <- 1..x, y <- 1..y, do: {x, y}
+  end
 
+  defp palette(:all), do: [:i, :l, :y, :n, :p, :w, :u, :v, :s, :f, :x, :t]
+  defp palette(:small), do: [:u, :v, :p]
+  defp palette(:medium), do: [:t, :y, :l, :p, :n, :v, :u]
+
+  def pick(board, :board), do: board
   def pick(%{active_pento: pento} = board, sname) when not is_nil(pento) do
     if pento.name == sname do
       %{board | active_pento: nil}
@@ -62,12 +76,14 @@ defmodule Pento.Game.Board do
   def pick(board, shape_name) do
     active =
       board.completed_pentos
-      |> Enum.find(& (&1.name == shape_name))
-      |> Kernel. || (new_pento(board, shape_name))
+      |> Enum.find(&(&1.name == shape_name))
+      |> Kernel.||(new_pento(board, shape_name))
 
-    completed = Enum.filter(& (&1.name == shape_name))
+    completed = Enum.filter(board.completed_pentos, &(&1.name != shape_name))
+
     %{board | active_pento: active, completed_pentos: completed}
   end
+
 
   defp new_pento(board, shape_name) do
     Pentomino.new(name: shape_name, location: midpoints(board))
@@ -88,7 +104,8 @@ defmodule Pento.Game.Board do
     |> Map.put(:completed_pentos, [pento | board.completed_pentos])
   end
 
-    def legal_move?(%{active_pento: pento, points: points} = _board) do
+
+  def legal_move?(%{active_pento: pento, points: points} = _board) do
     pento.location in points
   end
 
@@ -105,5 +122,5 @@ defmodule Pento.Game.Board do
 
     points_on_board and no_overlapping_pentos
   end
-  
+
 end
